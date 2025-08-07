dd3c0b3388c852b28ff21f2d9e6a2655e3b33de9
vulpes-a <vulpesily@gmail.com>
2025-08-07 11:30:07 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,41 +1,69 @@
 #include <stdio.h>
 
+#define LINHA 10
+#define COLUNA 10
+#define TAMANHO_NAVIO 3
+#define AGUA 0
+
 int main() {
-    int mar[10][10] = {0};
-    char colunas[] = "ABCDEFGHIJ";
 
-    // Barco horizontal
-    int linhaH = 1; 
-    int colInicioH = 2;
-    for (int i = 0; i < 5; i++) {
-        mar[linhaH][colInicioH + i] = 3;
-    }
+    //Variável que será usada para numeração das colunas
+    int v = 0;
 
-    // Barco vertical
-    int colV = 5; 
-    int linhaInicioV = 4; 
-    for (int i = 0; i < 3; i++) {
-        mar[linhaInicioV + i][colV] = 3;
-    }
+    //Criando matriz
+    int matriz[LINHA][COLUNA] = {0};
 
-    // Imprime o cabeçalho
+    char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    //Criando a coluna numérica de 1 - 10
     printf("   ");
-    for (int i = 0; i < 10; i++) {
-        printf("%c ", colunas[i]);
+    for (int j = 0; j < LINHA; j++){
+        printf("%c ", linha[j]);
     }
     printf("\n");
-
     printf("  +");
-    for (int i = 0; i < 10; i++) {
-        printf("--");
+    for (int j = 0; j < LINHA; j++){
+        printf("--", linha[j]);
     }
     printf("\n");
 
-    // Imprime a matriz
-    for (int linha = 0; linha < 10; linha++) {
-        printf("%2d|", linha + 1);
-        for (int col = 0; col < 10; col++) {
-            printf("%d ", mar[linha][col]);
+    //Navio vertical
+    int barcoL2 = 7;
+    int barcoC2 = 3;
+    for (int i = TAMANHO_NAVIO; i > 0; i--) {
+            matriz[barcoL2 + i - 1][barcoC2] = 3;
+    }
+
+    //Navio horizontal
+    int barcoL1 = 5;
+    int barcoC1 = 6;
+    for (int i = TAMANHO_NAVIO; i > 0; i--) {
+            matriz[barcoL1][barcoC1 + i - 1] = 3;
+        }
+
+    //Navio diagonal 1
+    int barcoL3 = 0;
+    int barcoC3 = 0;
+    for (int i = TAMANHO_NAVIO; i > 0; i--) {
+        for (int j = TAMANHO_NAVIO; j > 0; j--) {
+            matriz[barcoL3 + i - 1][barcoC3 + i - 1] = 3;
+        }
+    }
+
+    //Navio diagonal 2
+    int barcoL4 = 9;
+    int barcoC4 = 0;
+    for (int i = TAMANHO_NAVIO; i > 0; i--) {
+        for (int j = TAMANHO_NAVIO; j > 0; j--) {
+            matriz[barcoL4 - i][barcoC4 + i] = 3;
+        }
+    }
+
+    //Imprimindo a matriz
+    for (int i = 0; i < COLUNA; i++) {
+        printf("%d |", v++);
+        for (int j = 0; j < LINHA; j++) {
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
