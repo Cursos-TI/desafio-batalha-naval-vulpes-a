c5a5d9b4d711e3074fa30e48a88d302c9b1f09f8
vulpes-a <vulpesily@gmail.com>
2025-08-08 10:55:31 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,72 +1,90 @@
 #include <stdio.h>
 
-#define LINHA 10
-#define COLUNA 10
+#define LINHA 10      // Número de linhas do tabuleiro
+#define COLUNA 10     // Número de colunas do tabuleiro
 #define TAMANHO_NAVIO 3
-#define AGUA 0
+#define AGUA 0        // Representação da água
+#define NAVIO 3       // Representação do navio
+#define HABILIDADE 5  // Representação da área de habilidade
+#define ABS(x) ((x) < 0 ? -(x) : (x)) // Cálculo de valor absoluto sem bibliotecas extras
 
-int main() {
-
-    //Variável que será usada para numeração das colunas
-    int v = 0;
-
-    //Criando matriz
-    int matriz[LINHA][COLUNA] = {0};
-
-    char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
-    //Criando a coluna numérica de 1 - 10
+// Exibe o tabuleiro no console
+void imprimirTabuleiro(int matriz[LINHA][COLUNA]) {
+    char letras[LINHA] = {'A','B','C','D','E','F','G','H','I','J'};
     printf("   ");
-    for (int j = 0; j < LINHA; j++){
-        printf("%c ", linha[j]);
-    }
+    for (int j = 0; j < COLUNA; j++) printf("%c ", letras[j]);
+    printf("\n  +");
+    for (int j = 0; j < COLUNA; j++) printf("--");
     printf("\n");
-    printf("  +");
-    for (int j = 0; j < LINHA; j++){
-        printf("--", linha[j]);
-    }
-    printf("\n");
-
-    //Navio vertical
-    int barcoL2 = 7;
-    int barcoC2 = 3;
-    for (int i = TAMANHO_NAVIO; i > 0; i--) {
-            matriz[barcoL2 + i - 1][barcoC2] = 3;
-    }
-
-    //Navio horizontal
-    int barcoL1 = 5;
-    int barcoC1 = 6;
-    for (int i = TAMANHO_NAVIO; i > 0; i--) {
-            matriz[barcoL1][barcoC1 + i - 1] = 3;
-        }
-
-    //Navio diagonal 1
-    int barcoL3 = 0;
-    int barcoC3 = 0;
-    for (int i = TAMANHO_NAVIO; i > 0; i--) {
-        for (int j = TAMANHO_NAVIO; j > 0; j--) {
-            matriz[barcoL3 + i - 1][barcoC3 + i - 1] = 3;
-        }
-    }
-
-    //Navio diagonal 2
-    int barcoL4 = 9;
-    int barcoC4 = 0;
-    for (int i = TAMANHO_NAVIO; i > 0; i--) {
-        for (int j = TAMANHO_NAVIO; j > 0; j--) {
-            matriz[barcoL4 - i][barcoC4 + i] = 3;
-        }
-    }
-
-    //Imprimindo a matriz
-    for (int i = 0; i < COLUNA; i++) {
-        printf("%d |", v++);
-        for (int j = 0; j < LINHA; j++) {
+    for (int i = 0; i < LINHA; i++) {
+        printf("%2d|", i);
+        for (int j = 0; j < COLUNA; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+}
 
+// Gera matriz em forma de cone
+void gerarCone(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            matriz[i][j] = (j >= centro - i && j <= centro + i && i <= centro) ? 1 : 0;
+}
+
+// Gera matriz em forma de cruz
+void gerarCruz(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            matriz[i][j] = (i == centro || j == centro) ? 1 : 0;
+}
+
+// Gera matriz em forma de octaedro (losango)
+void gerarOctaedro(int tamanho, int matriz[tamanho][tamanho]) {
+    int centro = tamanho / 2;
+    for (int i = 0; i < tamanho; i++)
+        for (int j = 0; j < tamanho; j++)
+            matriz[i][j] = (ABS(i - centro) + ABS(j - centro) <= centro) ? 1 : 0;
+}
+
+// Aplica habilidade no tabuleiro
+void aplicarHabilidade(int tab[LINHA][COLUNA], int habTamanho, int hab[habTamanho][habTamanho], int origemL, int origemC) {
+    int centro = habTamanho / 2;
+    for (int i = 0; i < habTamanho; i++)
+        for (int j = 0; j < habTamanho; j++)
+            if (hab[i][j] == 1) { // Apenas nas áreas marcadas como 1
+                int tabL = origemL + (i - centro);
+                int tabC = origemC + (j - centro);
+                // Verifica limites do tabuleiro
+                if (tabL >= 0 && tabL < LINHA && tabC >= 0 && tabC < COLUNA)
+                    if (tab[tabL][tabC] == AGUA) // Não sobrescreve navio
+                        tab[tabL][tabC] = HABILIDADE;
+            }
+}
+
+int main() {
+    int tabuleiro[LINHA][COLUNA] = {0};
+
+    // Posiciona navios fixos
+    for (int i = 0; i < TAMANHO_NAVIO; i++) tabuleiro[7 + i][3] = NAVIO;
+    for (int i = 0; i < TAMANHO_NAVIO; i++) tabuleiro[5][6 + i] = NAVIO;
+    for (int i = 0; i < TAMANHO_NAVIO; i++) tabuleiro[0 + i][0 + i] = NAVIO;
+    for (int i = 0; i < TAMANHO_NAVIO; i++) tabuleiro[9 - i][0 + i] = NAVIO;
+
+    // Cria matrizes das habilidades
+    int cone[5][5], cruz[5][5], octaedro[5][5];
+    gerarCone(5, cone);
+    gerarCruz(5, cruz);
+    gerarOctaedro(5, octaedro);
+
+    // Aplica habilidades no tabuleiro em pontos fixos
+    aplicarHabilidade(tabuleiro, 5, cone, 2, 2);
+    aplicarHabilidade(tabuleiro, 5, cruz, 5, 5);
+    aplicarHabilidade(tabuleiro, 5, octaedro, 8, 8);
+
+    // Mostra tabuleiro final
+    imprimirTabuleiro(tabuleiro);
     return 0;
 }
